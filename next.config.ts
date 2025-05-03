4e0f5d7a06247af972b8937bf3b729246803c692
AchillesGit <dennisadiki@gmail.com>
2025-05-03 23:19:01 +0200

    Add headers configuration for Google Site Verification in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Google-Site-Verification",
+            value: "googlec9a9b45540e2246d.html",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
